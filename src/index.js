3795f36e5e2657368123863aae622f22efcfefa3
Mauricio Cartagena <mc973220@gmail.com>
2021-04-12 11:55:39 -0400

    Probando Docker

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("", (_, res) => res.send("Hello World"));
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log("up and running");
+});
